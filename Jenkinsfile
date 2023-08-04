d6a013fa781061d85efc63c5aae76edfcb6a9155
Ricardo Ferreira <rnhc1000@gmail.com>
2023-08-03 21:46:05 -0300

    Sync com o site

---
+++ Jenkinsfile
@@ -0,0 +1,11 @@
+node {
+  stage('SCM') {
+    checkout scm
+  }
+  stage('SonarQube Analysis') {
+    def scannerHome = tool 'SONAR_SCANNER'    
+    withSonarQubeEnv(credentialsId: 'SonarToken') {
+	    sh "${scannerHome}/bin/sonar-scanner"
+    }
+  }
+}
---
+++ portfolio.blade.php
@@ -0,0 +1,621 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Personal Porfolio</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swapp" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" />
+    <link rel="stylesheet" href="styles.css" />
+</head>
+
+<body>
+    <main>
+        <nav id="navbar">
+            <ul class="nav-list">
+                <li><a href="#welcome-section">Welcome</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#skills-section">Skills</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+        <section id="welcome-section" class="welcome-section">
+            <div class="welcome">
+                <h1>Welcome!!!</h1>
+                <hr />
+                <p>Thank you for touching down. I am a Full Stack Web Applications Developer,
+                    designing, coding and supporting the lifecycle of web-based payment applications using
+                    Laravel PHP, JS, HTML, CSS, Bootstrap, Git, GitHub, NodeJS, Docker, API, Spring Boot, MongoDB,
+                    MySQL, Nginx and others working fulltime as Sr Software Engineer.
+                </p>
+                <p> &#128071;</p>
+                <p>
+                    My name is
+                    <span class="rferreira">Ricardo Ferreira</span> and if you want to get down to the nitty-gritty
+                    of my challenging career as a solutions developer drpp me a quick message clicking on the social
+                    media links shown below or
+                    even take a look at my <span class="rferreira">Resume</span> clicking on the icon at the footer...&#128077;
+                </p>
+                <hr />
+            </div>
+        </section>
+        <section id="projects" class="projects-session">
+            <h2 class="projects-session-header">Here are a few of my latest projects</h2>
+            <div class="projects-grid">
+                <a href="https://sotech.com.br" target="_blank" class="project project-tile">
+                    <img class="project-image" src="fluxoTEF-2022-TT.png" alt="project">
+                    <p class="project-title">
+                        <span class="code">&lt;</span>
+                        &copy;simaTEF
+                        <span class="code">/&gt;</span>
+                    </p>
+                </a> <a href="https://dashboard.simatef.com.br" target="_blank" class="project project-tile">
+                    <img class="project-image" src="dashboard.png" alt="project">
+                    <p class="project-title">
+                        <span class="code">&lt;</span>
+                        Financial OPs
+                        <span class="code">/&gt;</span>
+                    </p>
+                </a>
+                <a href="https://easypay.simatef.com.br" target="_blank" class="project project-tile">
+                    <img class="project-image" src="easypayBackEnd.png" alt="project">
+                    <p class="project-title">
+                        <span class="code">&lt;</span>
+                        &copy;easyPAY backEnd
+                        <span class="code">/&gt;</span>
+                    </p>
+                </a>
+                <a href="https://easypag.simatef.com.br" target="_blank" class="project project-tile">
+                    <img class="project-image" src="easypayFrontEnd.png" alt="project">
+                    <p class="project-title">
+                        <span class="code">&lt;</span>
+                        &copy;easyPAY frontEnd
+                        <span class="code">/&gt;</span>
+                    </p>
+                </a>
+                <a href="https://alerta.simatef.com.br/nagios" target="_blank" class="project project-tile">
+                    <img class="project-image" src="devOps.png" alt="project">
+                    <p class="project-title">
+                        <span class="code">&lt;</span>
+                        devOps
+                        <span class="code">/&gt;</span>
+                    </p>
+                </a>
+                <a href="https://github.com/rnhc1000" target="_blank" class="project project-tile">
+                    <img class="project-image" src="springBoot.png" alt="project">
+                    <p class="project-title">
+                        <span class="code">&lt;</span>
+                        code
+                        <span class="code">/&gt;</span>
+                    </p>
+                </a>
+                <a href="https://mapadeclientes.simatef.com.br" target="_blank" class="project project-tile">
+                    <img class="project-image" src="mapa.png" alt="project">
+                    <p class="project-title">
+                        <span class="code">&lt;</span>
+                        GeoIP - Merchants
+                        <span class="code">/&gt;</span>
+                    </p>
+                </a>
+                <a href="https://zabbix.enormity.com.br" target="_blank" class="project project-tile">
+                    <img class="project-image" src="zbx.png" alt="project">
+                    <p class="project-title">
+                        <span class="code">&lt;</span>
+                       devOPs 
+                        <span class="code">/&gt;</span>
+                    </p>
+                </a>
+                <a href="https://notas.ferreiras.dev.br" target="_blank" class="project project-tile">
+                    <img class="project-image" src="tributo.png" alt="project">
+                    <p class="project-title">
+                        <span class="code">&lt;</span>
+                       tribute 
+                        <span class="code">/&gt;</span>
+                    </p>
+                </a>
+            </div>
+        </section>
+        <section id="skills-section" class="skills-section">
+            <div class="skills-section-header">
+                <h2>Here are some of my key skills</h2>
+            </div>
+            <div class="skills">
+                <ul>
+                    <li>
+                        BSc in Electrical Engineering by Sao Judas University, SP, BR
+                    </li>
+                    <li>
+                        PGDip in Computer Science by Mackenzie University, SP, BR
+                    </li>
+                    <li>
+                        PGDip in Cryptography, Fluminense Federal University, RJ, BR
+                    </li>
+                    <li>
+                        PGDip in Software Engineering, Unyleya University, SP, BR
+                    </li>
+                    <li>
+                        PGDip in Full Stack Web Development, Unyleya University, DF, BR
+                    </li>
+                    <li>
+                        Responsive Web Design by <a href="https://freecodecamp.org/certification/fccafad993d-1fad-465d-a551-3837fe575f0d/responsive-web-design" target="_blank">freecodecamp.org</a>
+                    </li>
+                    <li>
+                        Javascript- Algorithms and Data Structures by <a href="https://freecodecamp.org/certification/fccafad993d-1fad-465d-a551-3837fe575f0d/javascript-algorithms-and-data-structures" target="_blank">freecodecamp.org</a>
+                    </li>
+<li>
+  Javascript <a href="https://www.testdome.com/certificates/9c4c4b6b45c14f18b83802a08b844498" class="testdome-certificate-stamp gold">
+          <span class="testdome-certificate-name">Ricardo Alves Fer...</span>
+          <span class="testdome-certificate-test-name">JavaScript</span>
+          <span class="testdome-certificate-card-logo">TestDome<br>Certificate</span>
+      </a>
+</li>
+                </ul>
+            </div>
+        </section>
+        <section id="contact" class="contact-session">
+            <div class="contact-session-header">
+                <h2>What about working together...remotely?</h2>
+                <h4>Make a break and drop me a message, text, email or call me ...</h4>
+            </div>
+            <div class="contact-links">
+                <a href="https://twitter.com/Ricardo18101957" target="_blank" class="contact-details">Twitter
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a href="https://www.instagram.com/rnhc1000/" target="_blank" class="contact-details">Instagram
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/ricardo-ferreira-91b84a26" target="_blank"
+                    class="contact-details">Linkedin
+                    <i class="fab fa-linkedin-in"></i>
+                </a>
+                <a href="mailto:ricardo@ferreiras.dev.br" target="_blank" class="contact-details">
+                    <i class="fa fa-at"></i> Email
+                </a>
+                <a href="tel:+55 71 9 9300-5555" target="_blank" class="contact-details">
+                    <i class="fab fa-whatsapp"></i> Call me
+                </a>
+                <a href="tel:+55 71 9 9300-5555" target="_blank" class="contact-details">
+                    <i class="fab fa-telegram"></i> Telegram
+                </a>
+                <a href="https://github.com/rnhc1000" target="_blank" class="contact-details">
+                    <i class="fab fa-github"></i> Code
+                </a>
+                <a href="https://www.ferreiras.dev.br/cv/ResumeCVRev2022D.pdf" target="_blank"
+                    class="contact-details" target="_blank">
+                    <i class="fa fa-address-card"></i> Resume
+                </a>
+            </div>
+        </section>
+        <footer>
+            <p>&copy; 2023 - All Rights Reserved - Ricardo Ferreira</p>
+        </footer>
+    </main>
+</body>
+
+</html>
+
+<style>
+    /* Custom properties/variables  */
+    :root {
+        --main-white: #f0f0f0;
+        --main-red: #be3144;
+        --main-blue: #45567d;
+        --main-gray: #303841;
+        --main-orange: #F57513;
+        --orangered: rgb(255, 69, 0);
+    }
+
+    /* Base reset */
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+    /* box-sizing and font sizing */
+    *,
+    *::before,
+    *::after {
+        box-sizing: inherit;
+    }
+
+    html {
+        box-sizing: border-box;
+
+        /* Set font size for easy rem calculations
+   * default document font size = 16px, 1rem = 16px, 100% = 16px
+   * (100% / 16px) * 10 = 62.5%, 1rem = 10px, 62.5% = 10px
+  */
+        font-size: 62.5%;
+        scroll-behavior: smooth;
+    }
+
+    /* A few media query to set some font sizes at different screen sizes.
+ * This helps automate a bit of responsiveness.
+ * The trick is to use the rem unit for size values, margin and padding.
+ * Because rem is relative to the document font size
+ * when we scale up or down the font size on the document
+ * it will affect all properties using rem units for the values.
+*/
+
+    /* I am using the em unit for breakpoints
+ * The calculation is the following
+ * screen size divided by browser base font size
+ * As an example: a breakpoint at 980px
+ * 980px / 16px = 61.25em
+*/
+
+    /* 1200px / 16px = 75em */
+    @media (max-width: 75em) {
+        html {
+            font-size: 60%;
+        }
+    }
+
+    /* 980px / 16px = 61.25em */
+    @media (max-width: 61.25em) {
+        html {
+            font-size: 58%;
+        }
+    }
+
+    /* 460px / 16px = 28.75em */
+    @media (max-width: 28.75em) {
+        html {
+            font-size: 55%;
+        }
+    }
+
+    h1,
+    h2,
+    h4 {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 700;
+        text-align: center;
+        color: var(--main-orange);
+    }
+
+    h1 {
+        font-size: 6rem;
+    }
+
+    h2 {
+        font-size: 4.2rem;
+        font-style: italic;
+
+    }
+
+    h4 {
+        font-size: 2.0rem;
+    }
+
+    hr {
+        background-color: rgb(235, 182, 83);
+        height: 1px;
+        border: none;
+    }
+
+    hr.sep {
+        border-top: 0.5px dashed var(--orangered);
+    }
+
+    ul {
+        list-style: none;
+    }
+
+    a {
+        text-decoration: none;
+        color: var(--orangered);
+    }
+
+    body {
+        margin: 0;
+        width: 100%;
+        height: 100vh;
+        background: linear-gradient(115deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url(https://cdn.pixabay.com/photo/2016/03/27/19/50/light-1283987_960_720.jpg), no-repeat;
+        font-family: "Poppins", sans-serif;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+
+    /* nav */
+    nav {
+        display: flex;
+        justify-content: flex-end;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        background: transparent;
+        color: linen;
+        box-shadow: 0 2px 0 rgba(0, 0, 0, 0.4);
+        z-index: 10;
+        border: none;
+    }
+
+    .nav-list {
+        display: flex;
+        margin-right: 2rem;
+        border: none;
+    }
+
+    @media (max-width: 28.75em) {
+        nav {
+            justify-content: center;
+        }
+
+        .nav-list {
+            margin: 0 1rem;
+        }
+    }
+
+    .nav-list a {
+        display: block;
+        font-size: 2.2rem;
+        padding: 2rem;
+        color: var(--orangered);
+        border: none;
+    }
+
+    .nav-list a:hover {
+        transform: translateY(8px);
+        font-weight: 800;
+        transition: color 0.5s ease-out;
+
+    }
+
+    /* Welcome section */
+
+    .welcome-section {
+        display: flex;
+        flex-direction: column;
+        justify-content: left;
+        align-items: left;
+        width: 100%;
+        height: 100vh;
+        background-color: transparent;
+        z-index: -1;
+        margin: 0 auto;
+        padding: 20px;
+    }
+
+    .welcome {
+        font-size: 2rem;
+        font-weight: 200;
+        font-style: italic;
+        color: linen;
+        position: absolute;
+        top: 100px;
+        width: 50%;
+        height: 100vh;
+        margin: 0 auto;
+    }
+
+    .first-paragraph::first-letter {
+        font-size: 4rem;
+        color: var(--orangered) float: left;
+        margin-right: 0.25rem;
+    }
+
+    .rferreira {
+        color: var(--orangered)
+    }
+
+    /* Projects section */
+
+    .projects-section {
+        text-align: center;
+        padding: 10rem 2rem;
+        background: var(--main-blue);
+    }
+
+    .projects-section-header {
+        max-width: 640px;
+        margin: 0 auto 6rem auto;
+        border-bottom: 0.2rem solid lightgray;
+    }
+
+    @media (max-width: 28.75em) {
+        .projects-section-header {
+            font-size: 4rem;
+        }
+    }
+
+    /* "Automagic" image grid using no media queries */
+    .projects-grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+        grid-gap: 4rem;
+        width: 100%;
+        max-width: 1280px;
+        margin: 0 auto;
+        margin-bottom: 6rem;
+    }
+
+    @media (max-width: 30.625em) {
+        .projects-section {
+            padding: 6rem 1rem;
+        }
+
+        .projects-grid {
+            grid-template-columns: 1fr;
+        }
+    }
+
+    .project {
+        background: var(--main-gray);
+        box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+        border-radius: 2px;
+    }
+
+    .code {
+        color: var(--main-gray);
+        transition: color 0.3s ease-out;
+    }
+
+    .project:hover .code {
+        color: #ff7f50;
+    }
+
+    .project-image {
+        height: calc(100% - 6.8rem);
+        width: 100%;
+        object-fit: cover;
+    }
+
+    .project-title {
+        font-size: 2rem;
+        padding: 2rem 0.5rem;
+    }
+
+    .btn {
+        display: inline-block;
+        padding: 1rem 2rem;
+        border-radius: 2px;
+    }
+
+    .btn-show-all {
+        font-size: 2rem;
+        background: var(--main-gray);
+        transition: transform 0.3s ease-out;
+    }
+
+    .btn-show-all:hover {
+        background: var(--main-red);
+    }
+
+    .btn-show-all:hover>i {
+        transform: translateX(2px);
+    }
+
+    .btn-show-all>i {
+        margin-left: 10px;
+        transform: translateX(0);
+        transition: transform 0.3s ease-out;
+    }
+
+
+    /* Skills section */
+
+    .skills-section {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        align-items: center;
+        width: 100%;
+        height: 50vh;
+        background: linear-gradient(115deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url(https://cdn.pixabay.com/photo/2016/03/27/19/50/light-1283987_960_720.jpg), no-repeat;
+/*
+        background-color: var(--main-gray);
+        */
+        z-index: -1;
+        margin: 0 auto;
+    }
+
+
+    .skills-section-header {
+        width: 100%;
+        margin: 0 auto 2rem auto;
+        border-bottom: 0.2rem solid lightgray;
+    }
+
+    .skills {
+        display: flex;
+        flex-direction: column;
+        align-content: center;
+        font-size: 2rem;
+        font-weight: 600;
+        font-style: italic;
+        color: var(--orangered);
+
+        width: 100%;
+        height: 100vh;
+        margin: 0 0 0 50%;
+    }
+
+    /* Contact section */
+
+    .contact-section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        width: 100%;
+        height: 80vh;
+        padding: 0;
+        margin: 0 auto;
+        background: linear-gradient(115deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6));
+        color: var(--orangered);
+    }
+
+    .contact-section-header>h2 {
+        font-size: 6rem;
+    }
+
+    @media (max-width: 28.75em) {
+        .contact-section-header>h2 {
+            font-size: 4rem;
+        }
+    }
+
+    .contact-section-header>p {
+        font-style: italic;
+    }
+
+    .contact-links {
+        display: flex;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+        width: 100%;
+        margin-top: 4rem;
+        flex-wrap: wrap;
+        margin-bottom: 2rem;
+        padding: 0 0 1rem 0;
+    }
+
+    .contact-details {
+        font-size: 2.4rem;
+        text-shadow: 2px 2px 1px #1f1f1f;
+        transition: transform 0.3s ease-out;
+    }
+
+    .contact-details:hover {
+        transform: translateY(8px);
+    }
+
+    .social-icons {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+
+    }
+
+
+    /* Footer */
+
+    footer {
+        font-weight: 800;
+        display: flex;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+        padding: 2rem;
+        background: linear-gradient(115deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url(https://cdn.pixabay.com/photo/2016/03/27/19/50/light-1283987_960_720.jpg), no-repeat;
+        /*background: linear-gradient(90deg, rgba(14, 14, 14, 1) 0%, rgba(61, 46, 20, 1) 90%, url(https://cdn.pixabay.com/photo/2016/03/27/19/50/light-1283987_960_720.jpg), no-repeat;);*/
+
+        border-top: 4px solid var(--orangered);
+        color: var(--orangered);
+        margin: 0 auto;
+    }
+
+    footer>p {
+        margin: 2rem;
+        text-align: center;
+    }
+
+    footer i {
+        vertical-align: middle;
+    }
+</style>
